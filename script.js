30c5f7a89d94fbb2608c10c6e215c34c516b6e37
pfd2mjp6tm-maker <pfd2mjp6tm@privaterelay.appleid.com>
2026-07-20 23:09:23 +0900

    script.js を更新

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// JavaScript
+function startLesson(){
+
+document.getElementById("home").style.display="none";
+
+document.getElementById("lesson").style.display="block";
+
+}
+
+document.getElementById("lesson").style.display="none";
+
+
