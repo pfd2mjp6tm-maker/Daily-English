219d2351b0657a54e9839a1bff1317c5b55ca1d2
pfd2mjp6tm-maker <pfd2mjp6tm@privaterelay.appleid.com>
2026-07-22 20:30:35 +0900

    questions.js を作成
    v0.5 Create questions data

---
+++ questions.js
@@ -0,0 +1,22 @@
+const questions = [
+  {
+    japanese: "昨日は何をしましたか？",
+    choices: [
+      "What did you do yesterday?",
+      "What are you doing today?",
+      "What will you do tomorrow?",
+      "Where did you go yesterday?"
+    ],
+    answer: 0
+  },
+  {
+    japanese: "今日は少し眠いです。",
+    choices: [
+      "I'm a little sleepy today.",
+      "I'm hungry today.",
+      "I'm busy today.",
+      "I'm excited today."
+    ],
+    answer: 0
+  }
+];
